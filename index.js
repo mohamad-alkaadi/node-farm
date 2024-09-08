8615bc27c33fb95e69fc26e54aeaf8cc7f52cde1
mohamad-alkaadi <mohamadalkaadi@gmail.com>
2024-09-08 11:48:07 +0400

    prettier config

--- index.js
+++ index.js
@@ -3,73 +3,77 @@
 // then the third party modules
 // then our own from the local file system
 
-const http = require("http")
-const url = require("url")
-const fs = require("fs")
+const http = require('http');
+const url = require('url');
+const fs = require('fs');
 // node js will automatically know to go and search in the node modules folder
-const slugify = require("slugify")
+const slugify = require('slugify');
 // slug is the last part of the url that contains a unique string that identifies the resource that the website is displaying
 // example: /product/fresh-avocados
 // the slug is (fresh-avocados)
 
-const replaceTemplate = require("./modules/replaceTemplate")
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8"
-)
+  'utf-8'
+);
 const tempProduct = fs.readFileSync(
   `${__dirname}/templates/template-product.html`,
-  "utf-8"
-)
+  'utf-8'
+);
 const tempCard = fs.readFileSync(
   `${__dirname}/templates/template-card.html`,
-  "utf-8"
-)
+  'utf-8'
+);
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
-const dataObj = JSON.parse(data)
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
+const dataObj = JSON.parse(data);
 
-const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }))
-console.log(slugs)
-// convert to lower case
-// console.log(slugify("Fresh Avocados", { lower: true }))
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
-  const { query, pathname } = url.parse(req.url, true)
+  const { query, pathname } = url.parse(req.url, true);
 
-  if (pathname === "/" || pathname === "/overview") {
-    res.writeHead(200, { "Content-type": "text/html" })
+  if (pathname === '/' || pathname === '/overview') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
     const cardsHtml = dataObj
       .map((el) => replaceTemplate(tempCard, el))
-      .join("")
-    console.log(cardsHtml)
-    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml)
+      .join('');
+    console.log(cardsHtml);
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
 
-    res.end(output)
+    res.end(output);
 
     //Product Page
-  } else if (pathname === "/product") {
-    res.writeHead(200, { "Content-type": "text/html" })
-    const product = dataObj[query.id]
-    const output = replaceTemplate(tempProduct, product)
-    res.end(output)
+  } else if (pathname === '/product') {
+    res.writeHead(200, {
+      'Content-type': 'text/html',
+    });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //API
-  } else if (pathname === "/api") {
-    res.writeHead(200, { "Content-type": "application/json" })
-    res.end(data)
+  } else if (pathname === '/api') {
+    res.writeHead(200, {
+      'Content-type': 'application/json',
+    });
+    res.end(data);
 
     //Not Found
   } else {
     res.writeHead(404, {
-      "Content-Type": "text/html",
-      "my-own-header": "hello-world",
-    })
-    res.end("<h1>page not found!</h1>")
+      'Content-Type': 'text/html',
+      'my-own-header': 'hello-world',
+    });
+    res.end('<h1>page not found!</h1>');
   }
-})
+});
 
-server.listen(8000, "127.0.0.1", () => {
-  console.log("listening to requests on port 8000")
-})
+server.listen(8000, '127.0.0.1', () => {
+  console.log('listening to requests on port 8000');
+});
